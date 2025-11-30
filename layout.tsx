1fcb80358bed4ea15d45c6e770a392a01bdab4e1
m67899932-boop <m67899932@gmail.com>
2025-11-30 04:36:09 -0300

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,2 @@
+export const metadata={title:"OS System"};
+export default function Root({children}){return <html><body className="min-h-screen">{children}</body></html>;}
---
+++ page.tsx
@@ -0,0 +1,1 @@
+export default function Home(){return <main className='p-6'>Sistema OS Iniciado</main>}
